9064b9d7dd88f25bb5569412d255466ca95d70f4
kartikayy007 <kartikayy6969@gmail.com>
2026-07-10 04:23:39 +0530

    intialised

---
+++ Kitsch/KitschApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitschApp.swift
+//  Kitsch
+//
+//  Created by kartikay on 10/07/26.
+//
+
+import SwiftUI
+
+@main
+struct KitschApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
